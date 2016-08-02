f2df789346af81747e96061a4c9662eb6701ac7d
Mukesh Arya <mukesh_arya@yahoo.com>
2016-08-02 16:56:44 +0000

    more

--- linux/threads01/example01.c
+++ linux/threads01/example01.c
@@ -24,9 +24,18 @@ int main (int argc, char ** argv) {
 		exit(0);
 	}
 
+/* below function are to deal with control structure
+       int pthread_attr_init(pthread_attr_t *attr);
+       int pthread_attr_destroy(pthread_attr_t *attr);
+*/
+
 	pthread_t pid01,pid02;
 	pthread_attr_t attr;
-	pthread_attr_init(&attr);
+	if (pthread_attr_init(&attr) != 0 ) {
+		perror("thread control structure initialization error");
+		exit(-1);
+	}
+
 	long limit = atol(argv[1]);
 
 	pthread_create(&pid01, &attr, sum_of_number, &limit);
@@ -38,6 +47,12 @@ int main (int argc, char ** argv) {
 	pthread_join(pid02, NULL);
 
 	printf("type ctrl-c to kill the process \n");
+	
+	pthread_attr_destroy(&attr);
+	if (pthread_attr_destroy(&attr) != 0 ) {
+		perror("thread control structure destroy error");
+		exit(-1);
+	}
 
 	for(;;);
 	exit(0);
