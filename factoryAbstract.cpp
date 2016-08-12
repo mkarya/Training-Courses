de6be34984e269327dfe223356a48a94b926260b
Mukesh Arya <mukesh_arya@yahoo.com>
2016-08-12 16:43:45 +0000

    thanks

--- factoryAbstract.cpp
+++ factoryAbstract.cpp
@@ -1,46 +1,22 @@
 #include <iostream>
 #include <vector>
+#include "factoryAbstract.h"
 
-using namespace std;
 
-
-class stooze {
-	public:
-		static stooze * make_stooge(int kk);
-		virtual void action() = 0;
-};
-
-class moe : public stooze {
-	public:
-		inline void action() {
-			cout << "roll eyes \n";
-		}
-};
-
-class joe : public stooze {
-	public:
-		inline void action() {
-			cout << "raise hand \n";
-		}
-};
-
-class ram : public stooze {
-	public:
-		inline void action() {
-			cout << "raise leg \n";
-		}
-};
-
-stooze * stooze::make_stooge(int xx) {
+stooze* stooze::make_stooge(int xx) {
+	stooze * _ptr;
 	switch (xx) {
 		case 1:
-			return new moe();
+			_ptr = new moe();
+			return _ptr;
 			break;
 		case 2:
-			return new joe();
+			_ptr = new joe();
+			return _ptr;
 			break;
 		case 3:
-			return new ram();
+			_ptr = new ram();
+			return _ptr;
 			break;
 		default:
 			return NULL;
@@ -49,28 +25,3 @@ stooze * stooze::make_stooge(int xx) {
 
 }
 
-int main (void) {
-	int input = 0;
-	vector<stooze *> ll;
-	cout << "enter choice, enter 0 to finish \n";
-	while (true) {
-		cin >> input; 
-
-		switch (input) {
-			case 1:
-			case 2:
-			case 3:
-				ll.push_back(stooze::make_stooge(input));
-				break;
-			default:
-				cout << "wrong entry \n";
-		}
-				
-		if (input == 0) break ;
-	}
-
-	for (int i = 0; i < ll.size(); i++) 
-		ll[i]->action();
-
-	return 0;
-}
